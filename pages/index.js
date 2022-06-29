313b8b953bc7948d41fe7beb041f024feba096ef
samailamalima <samuelmalima09@gmail.com>
2022-06-29 16:26:59 +0000

    commit on 'zaarpay'

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect} from "react";
+import CreateProduct from "../components/CreateProduct";
 import Product from "../components/Product";
+import HeadComponent from '../components/Head';
 
-import { useWallet } from '@solana/wallet-adapter-react';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 // Constants
-const TWITTER_HANDLE = 'malima_cloud';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
   const [products, setProducts] = useState([]);
+  
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
 
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+  
   useEffect(() => {
     if (publicKey) {
       fetch(`/api/fetchProducts`)
@@ -23,12 +37,6 @@ const App = () => {
     }
   }, [publicKey]);
 
-  const renderNotConnectedContainer = () => (
-    <div className="button-container">
-      <WalletMultiButton className="cta-button connect-wallet-button" />
-    </div>
-  );
-  
   const renderItemBuyContainer = () => (
     <div className="products-container">
       {products.map((product) => (
@@ -39,13 +47,21 @@ const App = () => {
 
   return (
     <div className="App">
+      <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header">Zaar Store </p>
+          <p className="header"> Zaar store</p>
           <p className="sub-text">The only Vegitable store that accepts shitcoins</p>
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
         </header>
 
         <main>
+          {creating && <CreateProduct />}
           {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
